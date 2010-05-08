b39ec5a7135b358bb7977f1ac237d6a270055bb9
Sirupsen <sirup@sirupsen.dk>
2010-05-08 21:25:32 +0200

    reverse

--- lib/ticketmaster/systems/github.rb
+++ lib/ticketmaster/systems/github.rb
@@ -1,9 +1,19 @@
 module TicketMaster
-  class Github
-    class Project
+  module Github
+    def self.included(model)
+      model.class_eval do
+        extend model::ClassMethods
+        include model::InstanceMethods
+      end
     end
 
-    class Tickets
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      def hai
+        "Hai from Github!"
+      end
     end
   end
 end
