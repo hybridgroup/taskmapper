9339f8dfdd5314336f6fda005fc31d99696ec23c
Sirupsen <sirup@sirupsen.dk>
2010-05-08 21:24:53 +0200

    Modules => classes

--- lib/ticketmaster/systems/github.rb
+++ lib/ticketmaster/systems/github.rb
@@ -1,19 +1,9 @@
 module TicketMaster
-  module Github
-    def self.included(model)
-      model.class_eval do
-        extend model::ClassMethods
-        include model::InstanceMethods
-      end
+  class Github
+    class Project
     end
 
-    module ClassMethods
-    end
-
-    module InstanceMethods
-      def hai
-        "Hai from Github!"
-      end
+    class Tickets
     end
   end
 end
