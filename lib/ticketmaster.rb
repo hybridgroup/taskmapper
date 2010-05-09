3f51dea606a36985852f1e2d90c4097614e185b0
Sirupsen <sirup@sirupsen.dk>
2010-05-09 21:15:29 +0200

    Candidate for skeleton

--- lib/ticketmaster.rb
+++ lib/ticketmaster.rb
@@ -17,75 +17,33 @@ class TicketMaster
     end
   end
 
-  class Ticket
-    def initialize(name, id, description)
-      @title, @id, @body = title, id, body
-    end
-  end
-
-  class Project
-    def initialize(name, id, description)
-      @title, @id, @body = title, id, body
-    end
-  end
-
   module Github
     class Tickets < TicketMaster::Tickets
-      def self.create
-        # Github way of creating a ticket
-        # Returns a ticket object
+      def self.create(title, body)
       end
 
-      def self.find
-        # Github way of finding tickets
-        # Returns an array of ticket objects
+      def self.find(id)
+      end
+
+      def self.update(id, status)
+      end
+
+      def self.delete(id, message)
       end
     end
 
     class Projects < TicketMaster::Projects
+      def self.create(title)
+      end
+
       def self.find(query)
-        # Github way of finding a project
-        # Returns an array of project objects
-      end
-    end
-
-    class Project < TicketMaster::Project
-      def initialize
-        @tickets = {1 => Ticket, 17 => Ticket, 23 => Ticket}
-        # Extend hash object with a find method so one could call
-        # project.tickets.find
-        @ticket = Ticket
-        # So one could call for class methods easily
-        # project.ticket.create
-      end
-    end
-
-    class Ticket < TicketMaster::Ticket
-      def initialize
-        super
-        create
       end
 
-      def self.create(title, options={})
-        # Create ticket
-        # Return self
+      def self.update(id, options = {})
       end
 
-      def status=(status)
-        # Set ticket status
-        # Close if :done
-        # Open if :in_progress
-      end
-
-      def close(option={})
-        # close with :message
+      def self.delete(id, message)
       end
     end
   end
 end
-
-p MyProject.methods
-github = TicketMaster.new(:github)
-p github
-
-#=> [:PrivateTicketApp, :Github, :Lighthouse]
