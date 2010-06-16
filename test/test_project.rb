ef18a8e4d948f92f3006bd20c6c0435db3913541
deadprogrammer <ron.evans@gmail.com>
2010-06-16 10:56:19 -0700

    Some fake tests

---
+++ test/test_project.rb
@@ -0,0 +1,12 @@
+require 'helper'
+
+class TestProject < Test::Unit::TestCase
+  context "a valid project" do
+    setup do
+    end
+
+    should "have the right properties" do
+      assert true
+    end
+  end
+end
--- test/test_ticketmaster.rb
+++ test/test_ticketmaster.rb
@@ -1,4 +1,18 @@
 require 'helper'
 
 class TestTicketmaster < Test::Unit::TestCase
+  context "a valid Ticketmaster provider" do
+    setup do
+      @ticketmaster = TicketMaster.new(:dummy, {:username => "John", :password => "seekrit"})
+    end
+
+    should "have the right properties" do
+      assert_instance_of TicketMaster, @ticketmaster
+
+      assert_equal "John", @ticketmaster.authentication.username
+      assert_equal "seekrit", @ticketmaster.authentication.password
+      assert_equal :dummy, @ticketmaster.system
+    end
+  end
+  
 end
