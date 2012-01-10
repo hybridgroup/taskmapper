960e8af9ae1ef63a160933708db4b5d6ae76c68d
Jim Gay <jim@saturnflyer.com>
2012-01-10 15:42:17 -0500

    update examples to clarify the source accounts
    This ticket makes minor alterations to the example code to make changing the provider much easier.

--- examples/tm_example_4.rb
+++ examples/tm_example_4.rb
@@ -5,11 +5,13 @@ require 'ticketmaster-lighthouse'
 
 # copy all tickets and comments from pivotal tracker to new lighthouse project (the easy way)
 pivotal = TicketMaster.new(:pivotal)
-pivotal_project = pivotal.project(97107)
 lighthouse = TicketMaster.new(:lighthouse)
 
-lighthouse_project = lighthouse.project!(:name => "Copy Test on #{Time.now}", 
-                                         :description => "A copy test")
-lighthouse_project.copy(pivotal_project)
+from = pivotal.project(97107)
+to = lighthouse.project!(:name => "Copy Test on #{Time.now}", 
+                          :description => "A copy test")
+                          
+to.copy(from)
+
 puts "Copy finished."
 
