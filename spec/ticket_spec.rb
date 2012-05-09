332499572e9c02ff2acfa97f6f72d94af1981d63
Rafael George <george.rafael@gmail.com>
2012-05-09 18:08:31 -0400

    refactor ticket_spec to be more rspec like

--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -4,69 +4,53 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 # Just replace the Dummy in @project_class and @ticket_class
 # Also, remember to mock or stub any API calls
 describe "Tickets" do
-  before(:each) do
-    @ticketmaster = TicketMaster.new(:dummy, {})
-    @project_class = TicketMaster::Provider::Dummy::Project
-    @ticket_class = TicketMaster::Provider::Dummy::Ticket
-    @project = @ticketmaster.projects.first
-  end
+  let(:tm) { TaskMapper.new(:dummy, {}) }
+  let(:project_class) { TaskMapper::Provider::Dummy::Project }
+  let(:ticket_class) { TaskMapper::Provider::Dummy::Ticket }
+  let(:project) { tm.projects.first }
 
   describe "for a Project" do
-    it "should return an array" do
-      @project.tickets.should be_an_instance_of Array
+    context "when #tickets" do 
+      subject { project.tickets }
+      it { should be_an_instance_of Array }
+      it { subject.first.should be_an_instance_of ticket_class }
     end
 
-    it "should return Ticket objects" do
-      @project.tickets.first.should be_an_instance_of @ticket_class
+    context "when searching wanting back all tickets that match the query" do 
+      subject { project.tickets([999]) }
+      it { should be_an_instance_of Array }
+      it { subject.first.should be_an_instance_of ticket_class }
+      it { subject.first.id.should be_eql(999) }
     end
 
-    describe "when searching wanting back all tickets that match the query" do
-      before do
-        @tickets = @project.tickets([999])
-      end
-
-      it "should return an array" do
-        @tickets.should be_an_instance_of Array
-      end
-
-      it "should return Ticket objects" do
-        @tickets.first.should be_an_instance_of @ticket_class
-      end
-
-      it "should return Tickets that match ID" do
-        @tickets.first.id.should == 999
-      end
-
-      it "should return an array when passing query hash" do
-        @project.tickets(:id => 999).should be_an_instance_of Array
-      end
+    context "when passing an query hash" do 
+      subject { project.tickets(:id => 999) }
+      it { should be_an_instance_of Array }
     end
 
-    describe "when searching wanting back the first ticket that matches the query" do
-      it "should return the first Ticket as a default" do
-        @project.ticket.should == TicketMaster::Provider::Dummy::Ticket
-      end
+    context "when searching wanting back the first ticket that matches the query" do 
+      subject { project.ticket }
+      it { should be_eql TaskMapper::Provider::Dummy::Ticket } 
+    end
 
-      describe "when querying using default ID query" do
-        it "should return a Ticket object" do
-          @project.ticket(888).should be_an_instance_of @ticket_class
-        end
+    context "when querying using default ID query" do 
+      subject { project.ticket(888) } 
+      it { should be_an_instance_of ticket_class }
+    end
 
-        it "should only return Tickets with the correct ID" do
-          @project.ticket(888).id.should == 888
-        end
-      end
+    context "when passing an id to #ticket" do 
+      subject { project.ticket(888) } 
+      it { subject.id.should be_eql(888) }
+    end
 
-      describe "when querying using hash" do
-        it "should return a Ticket object" do
-          @project.ticket(:id => 888).should be_an_instance_of @ticket_class
-        end
+    context "when passing a hash to #ticket" do 
+      subject { project.ticket(:id => 888) }
+      it { should be_an_instance_of ticket_class }
+    end
 
-        it "should only return Tickets with the correct ID" do
-          @project.ticket(:id => 888).id.should == 888
-        end
-      end
+    context "when passing an id to #ticket" do 
+      subject { project.ticket(888) } 
+      it { subject.id.should be_eql(888) }
     end
   end
-
 end
