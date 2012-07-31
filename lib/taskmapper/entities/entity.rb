36084efb60e9a25ae7c2043977408c369dbca3fc
Omar Rodriguez <omarjavier15@gmail.com>
2012-07-31 18:29:24 +0000

    Refactor add method satisfy(Hash) to entity

--- lib/taskmapper/entities/entity.rb
+++ lib/taskmapper/entities/entity.rb
@@ -2,6 +2,11 @@ module TaskMapper
   module Entities
     module Entity
       attr_accessor :id, :created_at, :updated_at
+      
+      def satisfy(given_attrs)
+        attrs = to_hash
+        attrs == attrs.merge(given_attrs)
+      end
     end
   end
 end
