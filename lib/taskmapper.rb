ec7045c8a153ab88e32899073c6ad5bd429c040f
Omar Rodriguez <omarjavier15@gmail.com>
2012-07-31 18:02:42 +0000

    Remove useless code

--- lib/taskmapper.rb
+++ lib/taskmapper.rb
@@ -7,13 +7,4 @@ require 'taskmapper/client'
 require 'taskmapper/factory'
 
 module TaskMapper
-  class << self
-    def new(provider_name, credentials)
-      session provider_name, credentials
-    end
-    
-    def session(provider_name, credentials)
-      Entities::Session.new provider_name, credentials
-    end
-  end
 end
