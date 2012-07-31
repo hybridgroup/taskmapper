33866f59b2ea9a9ad648570dd48d0e208a8e222a
Omar Rodriguez <omarjavier15@gmail.com>
2012-07-31 11:59:35 +0000

    Search projects with name containing 'Bored'

--- spec/search_project_spec.rb
+++ spec/search_project_spec.rb
@@ -5,30 +5,44 @@ describe "Search projects" do
     TaskMapper::Client.new :inmemory, :user => 'omar', :password => '1234'
   end
   
-  let(:search_results) { client.projects }
-  
-  context "Given the backend has 2 projects" do
+  context "Given the following projects" do
     before do
       client.project! :name => 'Awesome Project',
                       :description => 'This is awesome!'
       
       client.project! :name => 'Bored Project',
                       :description => 'This is bored'
+                      
+      client.project! :name => 'Extra Bored Project',
+                      :description => 'This is extra bored'
     end
     
-    context "Given no search criteria" do    
-      describe :search_results do
-        subject { search_results }
+    context "Retrieve all projects" do
+      let(:search_results) { client.projects }
       
-        its(:count) { should == 2 }
+      subject { search_results }
+    
+      its(:count) { should == 3 }
+    
+      describe :first do
+        subject { search_results.first }
+        its(:id) { should == 1 }
+        its(:name) { should == 'Awesome Project' }
+        its(:description) { should == 'This is awesome!' }
+        its(:created_at) { should be_a Time }
+      end
+    end
+    
+    context "Search projects with name containing 'Bored'" do
+      let(:search_results) { client.projects.select { |project| project.name.include? 'Bored' } }
       
-        describe :first do
-          subject { search_results.first }
-          its(:id) { should == 1 }
-          its(:name) { should == 'Awesome Project' }
-          its(:description) { should == 'This is awesome!' }
-          its(:created_at) { should be_a Time }
-        end
+      subject { search_results }
+      
+      its(:count) { should == 2 }
+      
+      describe :project_names do
+        subject { search_results.map { |p| p.name } }
+        it { should == ['Bored Project', 'Extra Bored Project'] }
       end
     end
   end
