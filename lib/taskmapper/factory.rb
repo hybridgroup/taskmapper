5cf47d7938eace65aab6fc04f992bacabaef0b4c
Omar Rodriguez <omarjavier15@gmail.com>
2012-07-31 22:21:29 +0000

    Unused methods removed from factory class

--- lib/taskmapper/factory.rb
+++ lib/taskmapper/factory.rb
@@ -6,36 +6,24 @@ module TaskMapper
     
     protected :provider_name=, 
       :credentials=,
+      :providers,
       :providers=
     
     def initialize(provider_name, credentials, options ={})
       self.provider_name = provider_name
       self.credentials = credentials
-      self.providers = {}
-      
-      self.projects_provider = options.fetch(:projects_provider) do
-        Providers::Provider.new self, :projects
-      end
-      
-      self.tasks_provider = options.fetch(:tasks_provider) do
-        Providers::Provider.new self, :tasks
-      end 
-    end
-    
-    def projects_provider=(provider)
-      providers[project_class] = provider
-    end
+      self.providers = {
+        Entities::Project => Providers::Provider.new(self, :projects),
+        Entities::Task    => Providers::Provider.new(self, :tasks) 
+      }
+    end    
     
     def projects_provider
       providers[project_class]
     end
-    
+
     def tasks_provider
       providers[task_class]
-    end
-    
-    def tasks_provider=(provider)
-      providers[task_class] = provider
     end
     
     def provider(entity_class)
@@ -50,12 +38,12 @@ module TaskMapper
       Entities::Project
     end
     
-    def session
-      Entities::Session.new self
+    def entity(entity_class, attrs)
+      entity_class.new attrs.merge(:factory => self)
     end
     
-    def project(attrs)
-      project_class.new attrs.merge(:factory => self)
+    def session
+      Entities::Session.new self
     end
     
     def projects
@@ -64,10 +52,6 @@ module TaskMapper
     
     def tasks(criteria = {})
       Repositories::Tasks.new self, criteria
-    end
-    
-    def entity(entity_class, attrs)
-      entity_class.new attrs.merge(:factory => self)
     end
     
     def provider_metadata
