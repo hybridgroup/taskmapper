9df75b15bb2ca263e44b6884d7f2832b3729d16f
Omar Rodriguez <omarjavier15@gmail.com>
2012-08-01 21:06:16 +0000

    Formatting

--- spec/create_project_spec.rb
+++ spec/create_project_spec.rb
@@ -11,12 +11,11 @@ describe "Create a new Project" do
     describe :project do
       subject { tm.project! attributes }
       
-      its(:id) { should == 1 }
-      its(:name) { should == 'test' }
-
+      its(:id)          { should == 1 }
+      its(:name)        { should == 'test' }
       its(:description) { should == 'this is a test'}
-      its(:created_at) { should be_a Time }
-      its(:updated_at) { should be_a Time }
+      its(:created_at)  { should be_a Time }
+      its(:updated_at)  { should be_a Time }
       it { should satisfy { |p| p.tasks.project.id == 1 } }
     end
   end
