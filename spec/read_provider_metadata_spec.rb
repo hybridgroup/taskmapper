3875f9bbe561462c0faf07b350573c5d5bc39ac8
Omar Rodriguez <omarjavier15@gmail.com>
2012-08-01 21:32:22 +0000

    Read metadata when not available

--- spec/read_provider_metadata_spec.rb
+++ spec/read_provider_metadata_spec.rb
@@ -1,28 +1,52 @@
 require 'spec_helper'
 
+module TaskMapper::Providers::NoMetaData
+  module Projects; end
+  module Tasks; end
+  module Comments; end
+end
+
 describe "Read Provider metadata" do
-  let(:tm) { TaskMapper.new :in_memory, { :user => 'foo', :password => 'bar' } } 
-  
-  context "Retrieve operations" do
-    subject { tm.metadata }
-    its(:operations) { should == {
-      :projects => [:create, :search, :find],
-      :tasks    => [:create, :search, :find]
-    }}
+  context "Given a provider with metadata available" do
+    let(:tm) { TaskMapper.new :in_memory, { :user => 'foo', :password => 'bar' } } 
+    
+    context "Retrieve operations" do
+      subject { tm.metadata }
+      its(:operations) { should == {
+        :projects => [:create, :search, :find],
+        :tasks    => [:create, :search, :find]
+      }}
+      
+      pending "metadata for comments"
+    end
+    
+    context "Support create projects?" do
+      subject { tm.support? :create, :projects }
+      it { should be_true }
+    end
+    
+    context "Support find projects?" do
+      subject { tm.support? :find, :projects }
+      it { should be_true }
+    end
+    
+    context "Support delete tasks" do
+      subject { tm.support? :delete, :tasks }
+      it { should_not be_true }
+    end
   end
   
-  context "Support create projects?" do
-    subject { tm.support? :create, :projects }
-    it { should be_true }
-  end
-  
-  context "Support find projects?" do
-    subject { tm.support? :find, :projects }
-    it { should be_true }
-  end
-  
-  context "Support delete tasks" do
-    subject { tm.support? :delete, :tasks }
-    it { should_not be_true }
+  context "Given a provider without metadata" do
+    let(:tm) { TaskMapper.new :no_metadata, { :user => 'foo', :password => 'bar' } } 
+    
+    context "Retrieve operations" do
+      subject { tm.metadata }
+      its(:operations) { should == {
+        :projects => [:no_metadata_available],
+        :tasks => [:no_metadata_available]
+      }}
+      
+      pending "metadata for comments"
+    end
   end
 end
