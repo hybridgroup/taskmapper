f42d2ffc0a370e26f8838f24b3901f5a3c19a3e8
Omar Rodriguez <omarjavier15@gmail.com>
2012-08-03 19:21:42 +0000

    Update a ticket Scenario valid attributes

---
+++ spec/update_project_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Update a Project" do
+  context "Given the Awesome Project exist" do
+    let(:tm) do
+      TaskMapper.new :in_memory, :user => 'omar', :password => '1234'
+    end
+    
+    let(:project) { tm.project! :name => 'P1', 
+                                :description => 'desc' }
+       
+    describe "Given I change the name" do
+      before(:all) do
+        project.name = "P2"
+        project.instance_eval do
+          updated_at = Time.new 1984, 02, 20
+        end
+      end
+      
+      describe :result do
+        subject { project.save }
+
+        it { should == true }
+      end
+      
+      describe :updated_project do
+        subject   { tm.projects.find_by_name "P2" }
+        
+        its(:id)          { should == project.id }
+        its(:updated_at)  { should satisfy { |t| t.day == Time.now.day } }
+      end
+    end
+  end
+end
