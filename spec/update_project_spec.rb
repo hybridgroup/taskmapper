b62083e534853e55340edbf8ca7dbf8c890d63a8
Omar Rodriguez <omarjavier15@gmail.com>
2012-08-04 11:16:13 +0000

    Add samples to test description update'

--- spec/update_project_spec.rb
+++ spec/update_project_spec.rb
@@ -6,27 +6,47 @@ describe "Update a Project" do
       TaskMapper.new :in_memory, :user => 'omar', :password => '1234'
     end
     
-    let(:project) { tm.project! :name => 'P1', 
-                                :description => 'desc' }
-       
-    describe "Given I change the name" do
+    let(:project) do 
+      p = tm.project! :name => 'Awesome Project', 
+                      :description => 'Something Awesome'
+      p.instance_eval do
+        updated_at = Time.new 1984, 02, 20
+      end
+      p
+    end
+    
+    describe "Given I change its name" do
       before(:all) do
-        project.name = "P2"
-        project.instance_eval do
-          updated_at = Time.new 1984, 02, 20
-        end
+        project.name = "Super Awesome Project"
       end
       
-      describe :result do
+      describe :save do
         subject { project.save }
-
         it { should == true }
       end
       
       describe :updated_project do
-        subject   { tm.projects.find_by_name "P2" }
+        subject   { tm.projects.find_by_name "Super Awesome Project" }
         
-        its(:id)  { should == project.id }
+        describe :updated_at do
+          subject { project.updated_at }
+          its(:day) { should == Time.now.day }
+        end
+      end
+    end
+    
+    describe "Given I change its description" do
+      before(:all) do
+        project.description = "Something Super Awesome"
+      end
+      
+      describe :save do
+        subject { project.save }
+        it { should == true }
+      end
+      
+      describe :updated_project do
+        subject   { tm.projects.find_by_description "Something Super Awesome" }
         
         describe :updated_at do
           subject { project.updated_at }
