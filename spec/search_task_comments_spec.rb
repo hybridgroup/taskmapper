43184a5abffd8b443ed74b4b79bd47f55a6f3ba7
Omar Rodriguez <omarjavier15@gmail.com>
2012-08-07 16:35:46 +0000

    Add pending spec for Search Task Comments, Scenario: all

---
+++ spec/search_task_comments_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Search task comments" do
+  let(:tm) do
+    TaskMapper.new :in_memory, :user => 'mark', :password => 'twain'
+  end
+
+  context "Given the learn_ukulele" do
+    let(:learn_ukulele) { tm.project! :name => 'Leard to play ukulele' }
+
+    context "And 'learn ukulele' have the 'buy ukulele' task" do
+      let(:buy_ukulele) do
+        learn_ukulele.create_task :title      =>  "Buy Ukulele",
+                                  :requestor  =>  "Me"
+      end
+
+      pending "And 'buy ukulele' have the following comments" do
+        before do
+          buy_ukulele.create_comment  :body => 'Researching ukuleles best price',
+                                      :author => 'Omar'
+
+          buy_ukulele.create_comment  :body => 'Found a good deal in X store',
+                                      :author => 'Omar'
+
+          buy_ukulele.create_comment  :body => 'Found a cheaper one in Y store',
+                                      :author => 'Omar'
+        end
+
+        context "When a retrieve all 'buy ukulele' comments" do
+          subject { buy_ukulele.coments }
+
+          its(:count) { should == 3 }
+
+          describe :second do
+            its(:id)      { should == 2 }
+            its(:body)    { should == 'Found a good deal in X store' }
+            its(:author)  { should == 'Omar' }
+          end
+        end
+      end
+    end
+  end
+end
