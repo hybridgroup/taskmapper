a4c2580c9acef26971d04f873e8591f7881c10d9
Rafael George <george.rafael@gmail.com>
2012-08-10 17:42:53 -0400

    Fix conflicts with merge

--- lib/taskmapper/entities/task.rb
+++ lib/taskmapper/entities/task.rb
@@ -52,7 +52,7 @@ module TaskMapper
       alias :comment! :create_comment
       
       def comments
-        factory.task_comments.where(:task_id => id)
+        factory.task_comments.where(:task => self)
       end
       
       def comments_count
